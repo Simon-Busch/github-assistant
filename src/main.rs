4fd94cadd3750fd11df1b2b44d17adac8d3a83a8
simon busch <simon.busch@live.fr>
2023-03-14 12:46:47 +0100

    add menu items + render ui + render issues list

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,23 @@ enum Event<I> {
   Tick,
 }
 
+#[derive(Copy, Clone, Debug)]
+enum MenuItem {
+    Home,
+    Issues,
+    PullRequests,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Issues => 1,
+            MenuItem::PullRequests => 2,
+        }
+    }
+}
+
 fn init_variables() -> (String, String) {
   dotenv().ok();
   let username = std::env::var("GITHUB_USERNAME").expect("GITHUB_USERNAME must be set.");
@@ -72,6 +89,123 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("{:?}", items.items);
     println!("{:?}", items.items[0].url);
     println!("{:?}", items.items[1].labels);
+
+    let menu_titles = vec!["Home","Issues", "PullRequests", "Quit"]; // vec!["Home", "Issues", "PR", "Quit"]
+    let mut active_menu_item = MenuItem::Home;
+    let mut pet_list_state = ListState::default();
+    pet_list_state.select(Some(0));
+    loop {
+      terminal.draw(|rect| {
+          let size = rect.size();
+          let chunks = Layout::default()
+              .direction(Direction::Vertical)
+              .margin(2)
+              .constraints(
+                  [
+                      Constraint::Length(3),
+                      Constraint::Min(2),
+                      Constraint::Length(3),
+                  ]
+                  .as_ref(),
+              )
+              .split(size);
+
+          let copyright = Paragraph::new("Github Assistant - All rights reserved")
+              .style(Style::default().fg(Color::LightCyan))
+              .alignment(Alignment::Center)
+              .block(
+                  Block::default()
+                      .borders(Borders::ALL)
+                      .style(Style::default().fg(Color::White))
+                      .title("Copyright")
+                      .border_type(BorderType::Plain)
+              )
+              ;
+
+          let menu = menu_titles
+              .iter()
+              .map(|t| {
+                  let (first, rest) = t.split_at(1);
+                  Spans::from(vec![
+                      Span::styled(
+                          first,
+                          Style::default()
+                              .fg(Color::Yellow)
+                              .add_modifier(Modifier::UNDERLINED),
+                      ),
+                      Span::styled(rest, Style::default().fg(Color::White)),
+                  ])
+              })
+              .collect();
+
+          let tabs = Tabs::new(menu)
+              .select(active_menu_item.into())
+              .block(Block::default().title("Menu").borders(Borders::ALL))
+              .style(Style::default().fg(Color::White))
+              .highlight_style(Style::default().fg(Color::Yellow))
+              .divider(Span::raw("|"));
+
+          rect.render_widget(tabs, chunks[0]);
+          match active_menu_item {
+              MenuItem::Home => rect.render_widget(render_home(), chunks[1]),
+              MenuItem::PullRequests => rect.render_widget(render_home(), chunks[1]),
+              MenuItem::Issues => {
+                  let pets_chunks = Layout::default()
+                      .direction(Direction::Horizontal)
+                      .constraints(
+                          [Constraint::Percentage(20), Constraint::Percentage(80)].as_ref(),
+                      )
+                      .split(chunks[1]);
+                  let (left, right) = render_issues(&items.items);
+                  rect.render_stateful_widget(left, pets_chunks[0], &mut pet_list_state);
+                  rect.render_widget(right, pets_chunks[1]);
+              }
+          }
+          rect.render_widget(copyright, chunks[2]);
+      })?;
+
+      match rx.recv()? {
+          Event::Input(event) => match event.code {
+              KeyCode::Char('q') => {
+                  disable_raw_mode()?;
+                  terminal.show_cursor()?;
+                  break;
+              }
+              KeyCode::Char('h') => active_menu_item = MenuItem::Home,
+              KeyCode::Char('i') => active_menu_item = MenuItem::Issues,
+              KeyCode::Char('p') => active_menu_item = MenuItem::PullRequests,
+              // KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
+              // KeyCode::Char('a') => {
+              //     add_random_pet_to_db().expect("can add new random pet");
+              // }
+              // KeyCode::Char('d') => {
+              //     remove_pet_at_index(&mut pet_list_state).expect("can remove pet");
+              // }
+              // KeyCode::Down => {
+              //     if let Some(selected) = pet_list_state.selected() {
+              //         let amount_pets = read_db().expect("can fetch pet list").len();
+              //         if selected >= amount_pets - 1 {
+              //             pet_list_state.select(Some(0));
+              //         } else {
+              //             pet_list_state.select(Some(selected + 1));
+              //         }
+              //     }
+              // }
+              // KeyCode::Up => {
+              //     if let Some(selected) = pet_list_state.selected() {
+              //         let amount_pets = read_db().expect("can fetch pet list").len();
+              //         if selected > 0 {
+              //             pet_list_state.select(Some(selected - 1));
+              //         } else {
+              //             pet_list_state.select(Some(amount_pets - 1));
+              //         }
+              //     }
+              // }
+              _ => {}
+          },
+          Event::Tick => {}
+      }
+  }
     Ok(())
 }
 
@@ -106,7 +240,79 @@ fn render_home<'a>() -> Paragraph<'a> {
   home
 }
 
+fn render_issues<'a>(issues:  &Vec<ApiResponseItem>) -> (List<'a>, Table<'a>) {
+  let pets = Block::default()
+      .borders(Borders::ALL)
+      .style(Style::default().fg(Color::White))
+      .title("Pets")
+      .border_type(BorderType::Plain);
 
+  // let pet_list = read_db().expect("can fetch pet list");
+  let items: Vec<_> = issues
+      .iter()
+      .map(|issue| {
+          ListItem::new(Spans::from(vec![Span::styled(
+              issue.title.clone(),
+              Style::default(),
+          )]))
+      })
+      .collect();
+
+  let selected_issue = &issues[0];
+
+  let list = List::new(items).block(pets).highlight_style(
+      Style::default()
+          .bg(Color::Yellow)
+          .fg(Color::Black)
+          .add_modifier(Modifier::BOLD),
+  );
+
+  let issue_details = Table::new(vec![Row::new(vec![
+      Cell::from(Span::raw(selected_issue.number.to_string())),
+      Cell::from(Span::raw(selected_issue.title.to_string())),
+      Cell::from(Span::raw(selected_issue.state.to_string())),
+      Cell::from(Span::raw(selected_issue.url.to_string())),
+      Cell::from(Span::raw(selected_issue.created_at.to_string())),
+  ])])
+  .header(Row::new(vec![
+      Cell::from(Span::styled(
+          "number",
+          Style::default().add_modifier(Modifier::BOLD),
+      )),
+      Cell::from(Span::styled(
+          "title",
+          Style::default().add_modifier(Modifier::BOLD),
+      )),
+      Cell::from(Span::styled(
+          "state",
+          Style::default().add_modifier(Modifier::BOLD),
+      )),
+      Cell::from(Span::styled(
+          "url",
+          Style::default().add_modifier(Modifier::BOLD),
+      )),
+      Cell::from(Span::styled(
+          "Created At",
+          Style::default().add_modifier(Modifier::BOLD),
+      )),
+  ]))
+  .block(
+      Block::default()
+          .borders(Borders::ALL)
+          .style(Style::default().fg(Color::White))
+          .title("Detail")
+          .border_type(BorderType::Plain),
+  )
+  .widths(&[
+      Constraint::Percentage(5),
+      Constraint::Percentage(20),
+      Constraint::Percentage(20),
+      Constraint::Percentage(5),
+      Constraint::Percentage(20),
+  ]);
+
+  (list, issue_details)
+}
 
 async fn get_github_response(username: &str, access_token: &str) -> Result<String, Box<dyn Error>> {
   let mut headers = header::HeaderMap::new();
