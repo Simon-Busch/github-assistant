cca21d86eba4dddfa6bcffdd969d093e7df1acf8
simon busch <simon.busch@live.fr>
2023-06-04 16:25:25 +0200

    fix error handle on comments

--- src/api/fetch_github_data.rs
+++ src/api/fetch_github_data.rs
@@ -7,47 +7,48 @@ use std::error::Error;
 
 
 pub async fn get_github_response(username: &str, access_token: &str, status: &str) -> Result<ApiResponse, Box<dyn Error>> {
-  let mut headers = header::HeaderMap::new();
-  headers.insert(
-      ACCEPT,
-      HeaderValue::from_static("application/vnd.github.v3+json"),
-  );
-  headers.insert(
-      "Authorization",
-      HeaderValue::from_str(&format!("Bearer {}", access_token)).unwrap(),
-  );
-  headers.insert("User-Agent", HeaderValue::from_static("my app"));
-  let client = reqwest::Client::builder()
-      .default_headers(headers)
-      .build()?;
-  let base_url = "https://api.github.com";
-  let url = format!(
-      "{}/search/issues?q=assignee:{}+state:{}&per_page=100
-      ",
-      base_url, username, status
-  );
-  let github_response = client
-      .get(url)
-      .send()
-      .await?
-      .text()
-      .await?;
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        ACCEPT,
+        HeaderValue::from_static("application/vnd.github.v3+json"),
+    );
+    headers.insert(
+        "Authorization",
+        HeaderValue::from_str(&format!("Bearer {}", access_token)).unwrap(),
+    );
+    headers.insert("User-Agent", HeaderValue::from_static("my app"));
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let base_url = "https://api.github.com";
+    let url = format!(
+        "{}/search/issues?q=assignee:{}+state:{}&per_page=100
+        ",
+        base_url, username, status
+    );
+    let github_response = client
+        .get(url)
+        .send()
+        .await?
+        .text()
+        .await?;
 
-  let mut items: ApiResponse = serde_json::from_str(&github_response)?;
-  for item in items.items.iter_mut() {
-      let url_parts: Vec<&str> = item.url.split("/").collect();
-      item.repository = Some(url_parts[url_parts.len() - 3].to_string());
-      item.organization = Some(url_parts[url_parts.len() - 4].to_string());
-      item.is_pr = url_parts.contains(&"pull");
-      if item.state == "open" {
-        let comments_url = &item.comments_url;
-        let comments_response = client.get(comments_url).send().await?;
-        if !comments_response.status().is_success() {
-            println!("Error: {}", comments_response.status());
+    let mut items: ApiResponse = serde_json::from_str(&github_response)?;
+    for item in items.items.iter_mut() {
+        let url_parts: Vec<&str> = item.url.split("/").collect();
+        item.repository = Some(url_parts[url_parts.len() - 3].to_string());
+        item.organization = Some(url_parts[url_parts.len() - 4].to_string());
+        item.is_pr = url_parts.contains(&"pull");
+        if item.state == "open" {
+            let comments_url = &item.comments_url;
+            let comments_response = client.get(comments_url).send().await?;
+                if !comments_response.status().is_success() {
+                    item.comments_list = vec![];
+                } else {
+                    let comments_json: Vec<IssueComments> = comments_response.json().await?;
+                    item.comments_list = comments_json;
+                }
         }
-        let comments_json: Vec<IssueComments> = comments_response.json().await?;
-        item.comments_list = comments_json;
-      }
-  }
-  Ok(items)
+    }
+    Ok(items)
 }
--- src/structs/api_response.rs
+++ src/structs/api_response.rs
@@ -1,7 +1,7 @@
 use super::ApiResponseItem;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct ApiResponse {
     pub total_count: i32,
     pub items: Vec<ApiResponseItem>,
